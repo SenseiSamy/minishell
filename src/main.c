b263ff90362c66f959cbdf0dc51f4ffabdb82d4c
SenseiSamy <samy.naji2003@gmail.com>
2023-04-22 19:14:23 +0200

    fix petit oublie dernier commmit

--- src/main.c
+++ src/main.c
@@ -6,46 +6,11 @@
 /*   By: snaji <snaji@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 16:24:32 by cfrancie          #+#    #+#             */
-/*   Updated: 2023/04/22 19:03:30 by snaji            ###   ########.fr       */
+/*   Updated: 2023/04/22 19:13:49 by snaji            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-t_cmd	*parsing(char *line)
-{
-	t_cmd	*cmds;
-
-	(void)line;
-	cmds = malloc(sizeof (t_cmd) * 3);
-	if (cmds == NULL)
-		return (NULL);
-	cmds[0].cmd = ft_strdup("cat");
-	cmds[0].args = malloc(2 * sizeof(char *));
-	cmds[0].args[0] = ft_strdup("cat");
-	cmds[0].args[1] = NULL;
-	cmds[0].redirect = malloc(2 * sizeof(char *));
-	cmds[0].redirect[0] = ft_strdup("< Makefile");
-	cmds[0].redirect[1] = NULL;
-
-	cmds[1].cmd = ft_strdup("grep");
-	cmds[1].args = malloc(3 * sizeof(char *));
-	cmds[1].args[0] = ft_strdup("grep");
-	cmds[1].args[1] = ft_strdup("cc");
-	cmds[1].args[2] = NULL;
-	cmds[1].redirect = malloc(1 * sizeof(char *));
-	cmds[1].redirect[0] = NULL;
-
-	cmds[2].cmd = ft_strdup("tr");
-	cmds[2].args = malloc(4 * sizeof(char *));
-	cmds[2].args[0] = ft_strdup("tr");
-	cmds[2].args[1] = ft_strdup("c");
-	cmds[2].args[2] = ft_strdup("x");
-	cmds[2].args[3] = NULL;
-	cmds[2].redirect = malloc(1 * sizeof(char *));
-	cmds[2].redirect[0] = NULL;
-	return (cmds);
-}
 
 int	main(int argc, char **argv, char **envp)
 {
