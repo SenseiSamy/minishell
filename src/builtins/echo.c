20e20f1035c1f8150cb0805cca954dd5e12b12b8
SenseiSamy <samy.naji2003@gmail.com>
2023-05-02 19:02:30 +0200

    changement echo afin de pouvoir executer des commandes comme 'echo -n -nn -n a'

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -3,46 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cfrancie <cfrancie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: snaji <snaji@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 21:04:34 by cfrancie          #+#    #+#             */
-/*   Updated: 2023/04/24 18:12:40 by cfrancie         ###   ########.fr       */
+/*   Updated: 2023/05/02 19:01:12 by snaji            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static bool	no_newline_args(char **args)
+static int	newline_args(int argc, char **args)
 {
+	int	n;
 	int	i;
 
-	if (args[1][0] != '-')
-		return (false);
-	i = 1;
-	while (args[1][i])
-		if (args[1][i++] != 'n')
-			return (false);
-	return (true);
+	n = 1;
+	while (n < argc)
+	{
+		if (args[n][0] != '-')
+			return (n);
+		i = 1;
+		while (args[n][i])
+			if (args[n][i++] != 'n')
+				return (n);
+		++n;
+	}
+	return (n);
 }
 
 int	ft_echo(int argc, char **argv)
 {
-	bool	no_newline;
 	int		start_arg;
+	bool	no_newline;
 
-	no_newline = false;
 	start_arg = 1;
-	if (argc > 1 && no_newline_args(argv))
-	{
+	no_newline = false;
+	if (argc > 1)
+		start_arg = newline_args(argc, argv);
+	if (start_arg > 1)
 		no_newline = true;
-		start_arg = 2;
-	}
 	while (start_arg < argc)
 	{
 		printf("%s", argv[start_arg]);
 		if (start_arg < argc - 1)
 			printf(" ");
-		start_arg++;
+		++start_arg;
 	}
 	if (!no_newline)
 		printf("\n");
