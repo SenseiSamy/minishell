a476648bc643d19877e0fe3435786dea9ac94321
SenseiSamy <samy.naji2003@gmail.com>
2023-05-02 18:31:30 +0200

    ajout erreur 'numerical argument required' dans exit

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,15 +6,27 @@
 /*   By: snaji <snaji@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 19:18:48 by snaji             #+#    #+#             */
-/*   Updated: 2023/05/02 16:29:41 by snaji            ###   ########.fr       */
+/*   Updated: 2023/05/02 18:22:22 by snaji            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
 
+static int	is_valid_arg(char *arg)
+{
+	int	i;
+
+	i = 0;
+	while (arg[i])
+		if (ft_isdigit(arg[i++]) == false)
+			return (false);
+	return (true);
+}
+
 int	ft_exit(t_exec *exec, char **args)
 {
-	int	status;
+	int		status;
+	t_cmd	*cmds;
 
 	status = 0;
 	if (args[1] != NULL && args[2] != NULL)
@@ -22,10 +34,19 @@ int	ft_exit(t_exec *exec, char **args)
 	if (args[1] == NULL)
 		ft_atoi(env_get_value("?"));
 	else
-		status = ft_atoi(args[1]);
+	{
+		if (is_valid_arg(args[1]) == true)
+			status = ft_atoi(args[1]);
+		else
+		{
+			error_message("exit", args[1], "numeric arguement required");
+			status = 2;
+		}
+	}
 	write(2, "exit\n", 5);
-	cleanup(exec->cmds);
+	cmds = exec->cmds;
 	free_exec(exec);
+	cleanup(exec->cmds);
 	env_free();
 	exit(status);
 }
