2873e15d190576829b29e9074d77931c3bb73e32
cfrancie <celeste.francier@protonmail.com>
2023-05-09 01:14:32 +0200

    mini leak

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cfrancie <cfrancie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 16:24:32 by cfrancie          #+#    #+#             */
-/*   Updated: 2023/05/08 22:14:03 by cfrancie         ###   ########.fr       */
+/*   Updated: 2023/05/09 01:13:44 by cfrancie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,8 @@ static void	utils_prompt(char *str, t_cmd *cmds)
 		exec(cmds);
 		cleanup(cmds);
 	}
+	else
+		free(line);
 }
 
 static int	ft_prompt(t_cmd *cmds)
