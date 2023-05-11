fcde47f1e3a7bbbf8422c6cbc473f4a7de9773de
cfrancie <celeste.francier@protonmail.com>
2023-05-11 02:31:23 +0200

    Merge branch 'main' of github.com:cfrancie/minishell

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cfrancie <cfrancie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 16:24:32 by cfrancie          #+#    #+#             */
-/*   Updated: 2023/05/10 19:15:50 by cfrancie         ###   ########.fr       */
+/*   Updated: 2023/05/11 01:37:48 by cfrancie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ static char	*get_prompt(void)
 			"\002", pwd);
 	if (is_crash(prompt))
 		return (NULL);
-	tmp = ft_strjoin(prompt, "$ \001\e[0m\002");
+	tmp = ft_strjoin(prompt, "\001\e[0m\002$ ");
 	if (is_crash(tmp))
 		return (NULL);
 	free(prompt);
--- src/parsing/utils_var.c
+++ src/parsing/utils_var.c
@@ -6,7 +6,7 @@
 /*   By: cfrancie <cfrancie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 18:17:08 by cfrancie          #+#    #+#             */
-/*   Updated: 2023/05/10 18:38:17 by cfrancie         ###   ########.fr       */
+/*   Updated: 2023/05/11 01:35:26 by cfrancie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ typedef struct s_var
 	size_t	res;
 	size_t	i;
 	char	*tmp;
+	char	quote;
 }			t_var;
 
 static size_t	lenvar(char *key)
@@ -47,31 +48,38 @@ static size_t	lenvar(char *key)
 	return (res + 1);
 }
 
+static void	ft_for_var(const char *str, t_var *var)
+{
+	var->i++;
+	var->tmp = get_name(str, var->i);
+	if (!var->tmp[0] || ft_isdigit(var->tmp[0]))
+	{
+		var->res++;
+		free(var->tmp);
+		return ;
+	}
+	if (env_get_value(var->tmp))
+		var->res += lenvar(env_get_value(var->tmp));
+	free(var->tmp);
+	while (str[var->i] && (ft_isalnum(str[var->i]) || str[var->i] == '_'))
+		var->i++;
+}
+
 size_t	calcule_res_size(const char *str)
 {
 	t_var	var;
 
-	var = (t_var){0, 0, NULL};
+	var = (t_var){0, 0, NULL, '\0'};
 	while (str[var.i])
 	{
-		if (str[var.i] == '$')
-		{
-			var.i++;
-			var.tmp = get_name(str, var.i);
-			if (!var.tmp[0] || ft_isdigit(var.tmp[0]))
-			{
-				var.res++;
-				free(var.tmp);
-				continue ;
-			}
-			if (env_get_value(var.tmp))
-				var.res += lenvar(env_get_value(var.tmp));
-			free(var.tmp);
-			while (str[var.i] && (isalnum(str[var.i]) || str[var.i] == '_'))
-				var.i++;
-			continue ;
-		}
-		var = (t_var){var.res + 1, var.i + 1, NULL};
+		if (!var.quote && (str[var.i] == '\"' || str[var.i] == '\''))
+			var.quote = str[var.i];
+		else if (var.quote && str[var.i] == var.quote)
+			var.quote = '\0';
+		if ((!var.quote || var.quote == '"') && str[var.i] == '$')
+			ft_for_var(str, &var);
+		var.res++;
+		var.i++;
 	}
 	return (var.res);
 }
