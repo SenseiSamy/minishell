f22c40b7f10c004388e335f858e69b564f225748
SenseiSamy <samy.naji2003@gmail.com>
2023-05-13 22:25:56 +0200

    fix exit

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: snaji <snaji@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 19:18:48 by snaji             #+#    #+#             */
-/*   Updated: 2023/05/03 16:13:28 by snaji            ###   ########.fr       */
+/*   Updated: 2023/05/13 22:25:42 by snaji            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static int	is_valid_arg(char *arg)
 	int	i;
 
 	i = 0;
-	if (ft_strlen(arg) == 1 && arg[0] == '-')
+	if (ft_strlen(arg) == 0 || (ft_strlen(arg) == 1 && arg[0] == '-'))
 		return (false);
 	while (arg[i])
 	{
@@ -34,21 +34,21 @@ int	ft_exit(t_exec *exec, char **args)
 	t_cmd	*cmds;
 
 	status = 0;
-	if (args[1] != NULL && args[2] != NULL)
-		return (error_message("exit", "too many arguments", NULL), 1);
+	write(2, "exit\n", 5);
 	if (args[1] == NULL)
-		ft_atoi(env_get_value("?"));
+		status = ft_atoi(env_get_value("?"));
 	else
 	{
-		if (is_valid_arg(args[1]) == true)
-			status = ft_atoi(args[1]);
-		else
+		if (is_valid_arg(args[1]) == false)
 		{
 			error_message("exit", args[1], "numeric arguement required");
 			status = 2;
 		}
+		else if (args[1] != NULL && args[2] != NULL)
+			return (error_message("exit", "too many arguments", NULL), 1);
+		else
+			status = ft_atoi(args[1]);
 	}
-	write(2, "exit\n", 5);
 	cmds = exec->cmds;
 	free_exec(exec);
 	cleanup(cmds);
